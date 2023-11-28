bf9d01c6e3a54239e60ad4900d8d815d0a35c66b
Dale Moncayo <dalemoncayo@gmail.com>
2023-11-28 23:33:01 +0800

    implementing tweet api in tweet branch

--- functions/index.js
+++ functions/index.js
@@ -8,94 +8,103 @@ admin.initializeApp();
 const firestore = admin.firestore();
 const app = express();
 
+const tweet = 'tweet';
+
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
 
 // Middleware to parse JSON
 app.use(express.json());
 
-// Create a post
-app.post('/post', async (req, res) => {
+// Create a tweet
+app.post('/' + tweet + '/:studentId', async (req, res) => {
   try {
-    const { description } = req.body;
+    const studentId = req.params.studentId;
+    const { name, description } = req.body;
     const timestamp = admin.firestore.FieldValue.serverTimestamp();
 
-    const newPost = await firestore.collection('post').add({
+    const newTweet = await firestore.collection(tweet + '_' + studentId).add({
+      name,
       description,
       timestamp,
     });
 
-    return res.status(201).json({ id: newPost.id, message: 'Post created successfully' });
+    return res.status(201).json({ id: newTweet.id, message: 'Tweet created successfully' });
   } catch (error) {
-    console.error('Error creating post:', error);
+    console.error('Error creating tweet:', error);
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-// Read a post
-app.get('/post/:postId', async (req, res) => {
+// Read a tweet
+app.get('/' + tweet + '/:studentId/:tweetId', async (req, res) => {
   try {
-    const postId = req.params.postId;
-    const postnapshot = await firestore.collection('post').doc(postId).get();
+    const tweetId = req.params.tweetId;
+    const studentId = req.params.studentId;
+    const tweetSnapshot = await firestore.collection(tweet + '_' + studentId).doc(tweetId).get();
 
-    if (!postnapshot.exists) {
-      return res.status(404).json({ error: 'Post not found' });
+    if (!tweetSnapshot.exists) {
+      return res.status(404).json({ error: 'Tweet not found' });
     }
 
-    const postData = postnapshot.data();
-    return res.status(200).json({ id: postnapshot.id, ...postData });
+    const tweetData = tweetSnapshot.data();
+    return res.status(200).json({ id: tweetSnapshot.id, ...tweetData });
   } catch (error) {
-    console.error('Error reading post:', error);
+    console.error('Error reading tweet:', error);
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-// Read all post
-app.get('/post', async (req, res) => {
+// Read all tweet
+app.get('/' + tweet + '/:studentId', async (req, res) => {
   try {
-    const postSnapshot = await firestore.collection('post').orderBy('timestamp').get();
+    const studentId = req.params.studentId;
+    const tweetSnapshot = await firestore.collection(tweet + '_' + studentId).orderBy('timestamp').get();
 
-    const allpost = [];
-    postSnapshot.forEach((doc) => {
-      const postData = doc.data();
-      allpost.push({ id: doc.id, ...postData });
+    const allTweet = [];
+    tweetSnapshot.forEach((doc) => {
+      const tweetData = doc.data();
+      allTweet.push({ id: doc.id, ...tweetData });
     });
 
-    return res.status(200).json(allpost);
+    return res.status(200).json(allTweet);
   } catch (error) {
-    console.error('Error reading all post:', error);
+    console.error('Error reading all tweet:', error);
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-// Update a post
-app.put('/post/:postId', async (req, res) => {
+// Update a tweet
+app.put('/' + tweet + '/:studentId/:tweetId', async (req, res) => {
   try {
-    const postId = req.params.postId;
-    const { description } = req.body;
+    const tweetId = req.params.tweetId;
+    const studentId = req.params.studentId;
+    const { name, description } = req.body;
     const timestamp = admin.firestore.FieldValue.serverTimestamp();
 
-    await firestore.collection('post').doc(postId).update({
+    await firestore.collection(tweet + '_' + studentId).doc(tweetId).update({
+      name,
       description,
       timestamp,
     });
 
-    return res.status(200).json({ message: 'Post updated successfully' });
+    return res.status(200).json({ message: 'Tweet updated successfully' });
   } catch (error) {
-    console.error('Error updating post:', error);
+    console.error('Error updating tweet:', error);
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-// Delete a post
-app.delete('/post/:postId', async (req, res) => {
+// Delete a tweet
+app.delete('/' + tweet + '/:studentId/:tweetId', async (req, res) => {
   try {
-    const postId = req.params.postId;
-    await firestore.collection('post').doc(postId).delete();
+    const tweetId = req.params.tweetId;
+    const studentId = req.params.studentId;
+    await firestore.collection(tweet + '_' + studentId).doc(tweetId).delete();
 
-    return res.status(200).json({ message: 'Post deleted successfully' });
+    return res.status(200).json({ message: 'Tweet deleted successfully' });
   } catch (error) {
-    console.error('Error deleting post:', error);
+    console.error('Error deleting tweet:', error);
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 });
